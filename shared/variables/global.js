dc86928b1e711e145b6ac31694cad016bdb0d1f5
Shahriar Parvez <mr.spshuvo@gmail.com>
2022-07-12 17:09:29 +0600

    :zap: add local and global variables scope on shared/variables dir

---
+++ shared/variables/global.js
@@ -0,0 +1,112 @@
+export const COMPANY = 'Agora';
+export const URL_ROOT = 'https://docs.agora.io/en';
+export const AGORA_BACKEND = `${COMPANY} SD-RTN`;
+export const CLIENT = platform => (platform === 'unity' ? 'game' : 'app');
+
+export const CONSOLE = `${COMPANY} Console`;
+export const TOKEN = 'token';
+
+export const ENGINE = `${COMPANY} Engine`;
+export const RTE = 'Real-Time Engagement';
+export const RTEC = `${RTE} Core`;
+export const RTES = `${RTE} SDK`;
+export const APP_ID_LINK = `http://agora.io`;
+export const AGORA_CONSOLE_URL = `http://console.agora.io`;
+export const API_REF_ANDROID = `http://api.agora.io/android`;
+
+export const PLATFORM = 'dummy';
+
+export const VIDEO = 'Video Calling';
+export const AUDIO = 'Voice Calling';
+export const ILS = 'Interactive Live Streaming';
+export const ILSS = 'Interactive Live Streaming Standard';
+export const ILSP = 'Interactive Live Streaming Premium';
+export const VSDK = 'Video SDK';
+export const VSDK_RELEASE = '4.x';
+export const VSDK_PREVIOUS_RELEASE = '3.xs';
+
+export const AV = 'Audio/Video';
+export const AV_URL = `${URL_ROOT}/video`;
+
+export const MESS = 'Real-Time Messaging';
+export const MESS_SDK = 'RTM SDK';
+export const SIG = `${MESS}`;
+export const CHAT = `${COMPANY} Chat`;
+export const PS = 'Pub-Sub';
+export const PUSH = 'Push Notifications';
+
+export const ACC = 'Acceleration';
+export const ACCM = 'Media Accelerator';
+export const ACCG = 'Global Accelerator';
+
+export const TEMPL = 'Templates';
+
+export const RTEE = 'Extensions';
+export const AA = `${COMPANY} Analytics`;
+export const WHITE = 'Interactive Whiteboard';
+export const EM = `${RTEE} Marketplace`;
+export const MS = 'Media Services';
+export const REC = 'Recording';
+export const MOD = 'Moderation';
+export const CP = 'Cloud Proxy';
+export const RTMPC = 'RTMP Converter';
+export const TRANS = 'Transcoding';
+export const OPREC = 'On-premise Recording';
+export const CREC = 'Cloud Recording';
+
+export const UCS = 'Use Cases';
+
+export const AB = 'App Builder';
+export const UIK = 'UIKit';
+export const FC = 'Flexible Classroom';
+
+export const WEB = 'Web';
+export const IOS = 'iOS';
+export const MAC = 'MacOS';
+export const AND = 'Android';
+export const WIN = 'Windows';
+
+export const ELE = 'Electron';
+export const UNI = 'Unity';
+export const FLU = 'Flutter';
+export const REA = 'React Native';
+export const COC = 'Cocos Creator';
+export const CO2_AN = 'Cocos2d-x-Android';
+export const SS = 'Server side';
+
+export const ATW = 'Technical Writer';
+export const ADR = 'DevReller';
+export const ADEV = 'Developer';
+export const AQA = 'Tester';
+export const APO = 'Product Owner';
+
+export const REPO_URL = 'https://gitlab.com/billy-the-fish/asciidoc-test';
+export const AGORA_DOCS_URL = 'https://docs.agora.io/en';
+export const DOWNLOAD_URL = 'https://download.agora.io/sdk/release';
+export const AGORA_PLATFORM_FOLDER = 'Agora%20Platform';
+
+export const API_REF_FOLDER = 'API%20Reference';
+export const API_REF_WEB = 'https://api.agora.io/web';
+
+export const APP_LINK = `${AGORA_DOCS_URL}/${AGORA_PLATFORM_FOLDER}/get_appid_token#get-the-app-id[App ID]`;
+
+export const APP_CERTIFICATE_LINK =
+  'xref:{rootdir}/reference/manage-agora-account.adoc#_get_the_app_certificate[App Certificate]';
+
+export const DEMO_BASIC_VIDEO_CALL_URL =
+  'https://webdemo.agora.io/agora-web-showcase/examples/Agora-Web-Tutorial-1to1-Web/';
+
+export const DEMO_PAGE_LINK = `${DEMO_BASIC_VIDEO_CALL_URL}[${COMPANY} web demo,role=external,window=_blank]`;
+
+export const TOKEN_GENERATE_LINK = `xref:{rootdir}/reference/manage-agora-account.adoc#_generate_a_temporary_token[Generate a temporary token]`;
+
+export const AGORA_DYNAMIC_KEY_CODE_BASE_URL =
+  'https://github.com/AgoraIO/Tools/tree/master/DynamicKey/AgoraDynamicKey';
+
+export const ANDROID_UIKIT_VERSION = '4.0.1';
+export const ANDROID_UIKIT_SDK = 'com.github.AgoraIO-Community.Android-UIKit:final';
+
+export const WEB_UIKIT = 'Agora React Web UIKit';
+
+export const IOS_MI_PACKAGE_VERSION = '4.0.0-beta.1';
+export const IOS_PACKAGE = 'NAME: AgoraRtcEngine_iOS_Beta';
---
+++ shared/variables/local.js
@@ -0,0 +1,21 @@
+const data = {
+  'video-calling': {
+    PRODUCT: 'Video Calling',
+    CLIENT: 'app',
+    PATH: 'video-calling'
+  },
+
+  'voice-calling': {
+    PRODUCT: 'Voice Calling',
+    CLIENT: 'app',
+    PATH: 'voice-calling'
+  },
+
+  'interactive-live-streaming': {
+    PRODUCT: 'Interactive Live Streaming',
+    CLIENT: 'app',
+    PATH: 'interactive-live-streaming'
+  }
+};
+
+export default data;

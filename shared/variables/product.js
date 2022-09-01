dc700f05e1e1a201f48816892cda7a30ad4de02f
billy-the-fish <iain.cox@sarkdocumentation.com>
2022-09-01 12:53:16 +0200

    Name change for Media Pull, etc.

--- shared/variables/product.js
+++ shared/variables/product.js
@@ -1,12 +1,48 @@
 const data = {
-    'video-calling': {
-    NAME: 'Video Calling',
-    PATH: 'video-calling',
+    'analytics': {
+        NAME: 'Analytics',
+        PATH: 'agora-analytics',
+        SDK: 'Agora Analytics',
+        PRODUCT: 'Agora Analytics'
+    },
+
+    'cloud-recording': {
+        NAME: 'Cloud Recording',
+        PATH: 'cloud-recording',
+        SDK: 'Cloud Recording API',
+        PRODUCT: 'Cloud Recording'
+    },
+
+    'media-pull': {
+        NAME: 'Media Pull',
+        PATH: 'media-pull',
+        SDK: 'Media Pull API',
+        PRODUCT: 'Media Pull'
+    },
+
+    'media-push': {
+        NAME: 'Media Push',
+        PATH: 'media-push',
+        SDK: 'Media Push API',
+        PRODUCT: 'Media Push'
+    },
+
+  'interactive-live-streaming': {
+    NAME: 'Interactive Live Streaming',
+    PATH: 'interactive-live-streaming',
     SDK: 'Video SDK',
-    PRODUCT: 'Video Calling',
-    BETA_API_FOLDER: 'video-call-4.x-beta',
+    PRODUCT: 'Interactive Live Streaming',
     IOS_PACKAGE_NAME: 'AgoraRtcEngine_iOS_Beta'
   },
+
+    'video-calling': {
+        NAME: 'Video Calling',
+        PATH: 'video-calling',
+        SDK: 'Video SDK',
+        PRODUCT: 'Video Calling',
+        BETA_API_FOLDER: 'video-call-4.x-beta',
+        IOS_PACKAGE_NAME: 'AgoraRtcEngine_iOS_Beta'
+    },
 
   'voice-calling': {
     NAME: 'Voice Calling',
@@ -15,28 +51,6 @@ const data = {
     PRODUCT: 'Voice Calling'
   },
 
-  'interactive-live-streaming': {
-    NAME: 'Interactive Live Streaming',
-    PATH: 'interactive-live-streaming',
-    SDK: 'Video SDK',
-    PRODUCT: 'Interactive Live Streaming',
-    IOS_PACKAGE_NAME: 'AgoraRtcEngine_iOS_Beta'
-
-  },
-
-  'analytics': {
-      NAME: 'Agora Analytics',
-      PATH: 'agora-analytics',
-      SDK: 'Agora Analytics',
-      PRODUCT: 'Agora Analytics'
-  },
-
-  'cloud-recording': {
-      NAME: 'Cloud Recording',
-      PATH: 'cloud-recording',
-      SDK: 'Cloud Recording',
-      PRODUCT: 'Cloud Recording'
-  },
 
   'signalling': {
       NAME: 'Signalling',
@@ -44,6 +58,7 @@ const data = {
       SDK: 'Signalling SDK',
       PRODUCT: 'Signalling'
   }
+
 };
 
 export default data;

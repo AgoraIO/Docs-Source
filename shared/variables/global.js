1f677331e45c23a80183e294514edec9e9ae8290
Hussain khalid <hk9804211@gmail.com>
2022-09-28 23:17:41 +0500

    updates to correct build failed error

--- shared/variables/global.js
+++ shared/variables/global.js
@@ -35,7 +35,7 @@ export const API_REF_ANDROID_WHITE = `${API_REF_ROOT_WHITE}/android/2.x`;
 export const API_REF_IOS_WHITE = `${API_REF_ROOT_WHITE}/ios/2.x`;
 export const API_REF_WEB_WHITE = `${API_REF_ROOT_WHITE}/web/2.x`;
 export const API_REF_WEB_ROOT_PREV_SDK = `${API_REF_ROOT}/web/3.x`;
-export const API_REF_FLUTTER_ROOT_PREV_SDK = `${API_REF_ROOT}/flutter/${VSDK_FLUTTER_PREVIOUS_RELEASE}/API`;
+export const API_REF_FLUTTER_ROOT_PREV_SDK = `${API_REF_ROOT}/flutter/5.x/API`;
 export const CONSOLE = `${COMPANY} Console`;
 export const TOKEN = 'token';
 

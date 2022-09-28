f3d85be837d075fa58cf0f9d0a327e2f042c44d4
billy-the-fish <iain.cox@sarkdocumentation.com>
2022-09-28 11:30:05 +0200

    Lots of variable cleanup.

--- shared/variables/global.js
+++ shared/variables/global.js
@@ -1,5 +1,6 @@
+
 export const COMPANY = 'Agora';
-export const URL_ROOT = 'https://docs.agora.io/en';
+export const URL_ROOT = 'https://agoradocs.vercel.app/';
 export const AGORA_BACKEND = `${COMPANY} SD-RTN`;
 export const CLIENT = platform => (platform === 'unity' ? 'game' : 'app');
 
@@ -8,9 +9,10 @@ export const VSDK_FLUTTER_RELEASE_API = '6.x'
 export const VSDK_UNITY_FLUTTER_RN_RELEASE_API = '4.0.0.beta-2'
 export const VSDK_WEB_RELEASE_API = '4.13.0'
 export const MAJOR_VERSION = '4.x'
-export const API_REF_ROOT = 'https://agora-api-reference.vercel.app/video-sdk';
-export const API_REF_ROOT_VOICE_SDK = 'https://agora-api-reference.vercel.app/voice-sdk';
-export const API_REF_OLD = 'https://docs.agora.io/en/Video/API%20Reference';
+export const API_ROOT = 'https://agora-api-reference.vercel.app';
+export const API_REF_ROOT = `${API_ROOT}/video-sdk`;
+export const API_REF_ROOT_VIDEO_SDK = `${API_ROOT}/video-sdk`;
+export const API_REF_ROOT_VOICE_SDK = '${API_ROOT}/voice-sdk';
 export const API_REF_ANDROID_ROOT  = `${API_REF_ROOT}/android/${VSDK_RELEASE_API}/API`;
 export const API_REF_ANDROID_ROOT_VOICE_SDK = `${API_REF_ROOT_VOICE_SDK}/android/${VSDK_RELEASE_API}/API`;
 export const API_REF_WEB_ROOT = `${API_REF_ROOT}/web/${VSDK_RELEASE_API}`;
@@ -30,7 +32,7 @@ export const API_REF_ELECTRON_ROOT_VOICE_SDK  = `${API_REF_ROOT_VOICE_SDK}/elect
 export const API_REF_CPP_ROOT  = `${API_REF_ROOT}/cpp/${VSDK_RELEASE_API}/API`;
 export const API_REF_CPP_ROOT_VOICE_SDK  = `${API_REF_ROOT_VOICE_SDK}/cpp/${VSDK_RELEASE_API}/API`;
 export const API_REF_WINDOWS_CSHARP_ROOT  = `${API_REF_ROOT}/windows-csharp/3.6.2/API`;
-export const API_REF_ROOT_WHITE = 'https://agora-api-reference.vercel.app/interactive-whiteboard-sdk';
+export const API_REF_ROOT_WHITE = '${API_ROOT}/interactive-whiteboard-sdk';
 export const API_REF_ANDROID_WHITE = `${API_REF_ROOT_WHITE}/android/2.x`;
 export const API_REF_IOS_WHITE = `${API_REF_ROOT_WHITE}/ios/2.x`;
 export const API_REF_WEB_WHITE = `${API_REF_ROOT_WHITE}/web/2.x`;
@@ -46,7 +48,6 @@ export const RTES = `${RTE} SDK`;
 export const APP_ID_LINK = `http://agora.io`;
 export const AGORA_CONSOLE_URL = `http://console.agora.io`;
 export const API_REF_ANDROID = `http://api.agora.io/android`;
-export const LINK_APP_ID = `[Generate a temporary token](../reference/manage-agora-account#get-the-app-id)`;
 
 export const PLATFORM = 'dummy';
 
@@ -64,11 +65,22 @@ export const VSDK_FLUTTER_RELEASE = '6.0.0-rc.2';
 export const VSDK_FLUTTER_PREVIOUS_RELEASE = '5.x';
 
 export const AV = 'Audio/Video';
-export const AV_URL = `${URL_ROOT}/video`;
 
 export const MESS = 'Signaling';
 export const MESS_SDK = `${MESS} SDK`;
 export const SIG = `${MESS}`;
+export const SIG_RELEASE_API = `1.x`;
+export const SIG_SDK_API_REF_ROOT = `${API_ROOT}/signaling-sdk`;
+export const SIG_SDK_API_ANDROID = `${SIG_SDK_API_REF_ROOT}/android/${SIG_RELEASE_API}`;
+export const SIG_SDK_API_WIN_CPP = `${SIG_SDK_API_REF_ROOT}/windows-cpp/${SIG_RELEASE_API}`;
+export const SIG_SDK_API_LNX_CPP = `${SIG_SDK_API_REF_ROOT}/linux-cpp/${SIG_RELEASE_API}`;
+export const SIG_SDK_API_LNX_JAVA = `${SIG_SDK_API_REF_ROOT}/linux-java/${SIG_RELEASE_API}`;
+export const SIG_SDK_API_IOS = `${SIG_SDK_API_REF_ROOT}/ios/${SIG_RELEASE_API}`;
+export const SIG_SDK_API_MACOS = `${SIG_SDK_API_REF_ROOT}/macos/${SIG_RELEASE_API}`;
+export const SIG_SDK_API_WEB = `${SIG_SDK_API_REF_ROOT}/web/${SIG_RELEASE_API}`;
+export const SIG_SDK_API_REST = `https://agoradocs.vercel.app/signaling/reference/user-channel-events`;
+
+
 export const CHAT = `Chat`;
 export const PS = 'Pub-Sub';
 export const PUSH = 'Push Notifications';
@@ -132,26 +144,12 @@ export const ADEV = 'Developer';
 export const AQA = 'Tester';
 export const APO = 'Product Owner';
 
-export const REPO_URL = 'https://gitlab.com/billy-the-fish/asciidoc-test';
 export const AGORA_DOCS_URL = 'https://docs.agora.io/en';
-export const DOWNLOAD_URL = 'https://download.agora.io/sdk/release';
-export const AGORA_PLATFORM_FOLDER = 'Agora%20Platform';
-
-export const API_REF_FOLDER = 'API%20Reference';
-export const API_REF_WEB = 'https://docs.agora.io/en/Video/API%20Reference/web_ng';
-
-export const APP_LINK = `${AGORA_DOCS_URL}/${AGORA_PLATFORM_FOLDER}/get_appid_token#get-the-app-id[App ID]`;
-
-export const APP_CERTIFICATE_LINK =
-"https://agoradocs.vercel.app/video-calling/reference/manage-agora-account/#get-the-app-certificate";
 
 export const DEMO_BASIC_VIDEO_CALL_URL =
   'https://webdemo.agora.io/agora-websdk-api-example-4.x/basicVideoCall/index.html';
 
 export const DEMO_PAGE_LINK = `<Link to="{{Global.DEMO_BASIC_VIDEO_CALL_URL}}"><Vg k="COMPANY" /> web demo</Link>`;
-
-export const TOKEN_GENERATE_LINK = `<Link to="https://docs.agora.io/en/Agora%20Platform/get_appid_token?platform=All#generate-a-temporary-token">Generate a temporary token</Link>`;
-export const TOKEN_GENERATE_URL = `https://docs.agora.io/en/Agora%20Platform/get_appid_token?platform=All#generate-an-rtc-temporary-token`;
 
 export const AGORA_DYNAMIC_KEY_CODE_BASE_URL =
   'https://github.com/AgoraIO/Tools/tree/master/DynamicKey/AgoraDynamicKey';

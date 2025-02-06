6dbc2f01331f34a3b49ab1a517203191d8f8097d
Dasun Nirmitha <nirmithadasun@gmail.com>
2025-02-06 17:18:09 +0530

    ENDOC-607  On Premise Recording Explanation of Diagram Improvement (#1344)
    * ENDOC-607  On Premise Recording Explanation of Diagram Improvement
    
    * review-updates
    
    ---------
    
    Co-authored-by: saudsami <saudsami@gmail.com>

--- shared/variables/global.js
+++ shared/variables/global.js
@@ -209,6 +209,7 @@ export const CP = 'Cloud Proxy';
 export const RTMPC = 'RTMP Converter';
 export const TRANS = 'Transcoding';
 export const OPREC = 'On-Premise Recording';
+export const OPREC_SDK = `${OPREC} SDK`;
 export const STT = 'Speech to Text';
 export const OPREC_RELEASE_API = `3.x`;
 export const API_REF_ROOT_OPREC_SDK = `${API_ROOT}/on-premise-recording-sdk`;
